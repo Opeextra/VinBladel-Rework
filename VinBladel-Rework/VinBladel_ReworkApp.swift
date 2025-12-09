24ecf424fdfb422b2c97010222febc7c1769793d
Matthew Barton <matthewbarton2910@gmail.com>
2025-12-09 10:13:39 -0600

    Initial Commit

---
+++ VinBladel-Rework/VinBladel_ReworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinBladel_ReworkApp.swift
+//  VinBladel-Rework
+//
+//  Created by Matthew S. Barton on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct VinBladel_ReworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
