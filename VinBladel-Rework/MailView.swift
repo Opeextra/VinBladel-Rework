cb3941b021ac6dbbcff62da8eb9419624d23db92
LuffyGear5 <ashah8399@stu.d214.org>
2026-01-21 11:13:58 -0600

    Finished mail

--- VinBladel-Rework/MailView.swift
+++ VinBladel-Rework/MailView.swift
@@ -9,22 +9,28 @@ import SwiftUI
 import MessageUI
 
 struct MailView : UIViewControllerRepresentable {
+    @Binding var isPresented: Bool
     func makeUIViewController(context: Context) -> MFMailComposeViewController {
         let mailVC = MFMailComposeViewController()
         mailVC.setToRecipients(["ashah8399@stu.d214.org"])
         mailVC.setSubject("invoice")
         mailVC.setMessageBody("Hello", isHTML: false)
+        mailVC.mailComposeDelegate = context.coordinator
         return mailVC
     }
     
     func updateUIViewController(_ uiViewController: MFMailComposeViewController, context: Context) {}
     
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(parent: self)
     }
     class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+        let parent : MailView
+        init(parent: MailView) {
+            self.parent = parent
+        }
         func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-            controller.dismiss(animated: true)
+            parent.isPresented = false
         }
     }
 }
