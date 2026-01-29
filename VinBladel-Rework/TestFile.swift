c273f9d8d8001824e8c528296d369bccff703066
LuffyGear5 <ashah8399@stu.d214.org>
2026-01-29 10:23:37 -0600

    Starting testing pdf generation again

--- VinBladel-Rework/TestFile.swift
+++ VinBladel-Rework/TestFile.swift
@@ -10,21 +10,22 @@ import MessageUI
 import UIKit
 
 struct TestFile: View {
-    @State private var showMail: Bool = false
+//    @State private var showMail: Bool = false
     
     var body: some View {
-        Button{
-            showMail = true
-        }label: {
-            Label("Send Mail", systemImage: "envelope")
-        }
-        .sheet(isPresented: $showMail) {
-            if MFMailComposeViewController.canSendMail(){
-                MailView(isPresented: $showMail)
-            }else{
-                Text("Not Available")
-            }
-        }
+//        Button{
+//            showMail = true
+//        }label: {
+//            Label("Send Mail", systemImage: "envelope")
+//        }
+//        .sheet(isPresented: $showMail) {
+//            if MFMailComposeViewController.canSendMail(){
+//                MailView(isPresented: $showMail)
+//            }else{
+//                Text("Not Available")
+//            }
+//        }
+        
     }
 }
 #Preview {
