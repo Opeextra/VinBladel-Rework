93f7e8ff062b51caf408e8ddb85709e259311d94
Matthew Barton <matthewbarton2910@gmail.com>
2026-02-12 10:40:33 -0600

    hersey husky logo got added, and the file got moved into the app file

--- VinBladel-Rework/LoadingAnimation.swift
+++ VinBladel-Rework/LoadingAnimation.swift
@@ -11,22 +11,32 @@ import SwiftUI
 struct loadingAnimation: View {
     @State var isAnimating = false
     var body: some View {
-        
-        Circle()
-            .trim(from: 0.2, to: 1.0)
-            .stroke(
-                Color.gray,
-                style: StrokeStyle(lineWidth: 5, lineCap: .round)
-            )
-            .frame(width: 40, height: 40)
-            .rotationEffect(.degrees(isAnimating ? 360 : 0))
-            .animation(
-                .linear(duration: 1)
-                .repeatForever(autoreverses: false),
-                value: isAnimating
-            )
-            .onAppear() {
-                isAnimating = true
-            }
+        VStack{
+            Image("hersey-husky-logo")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .padding(15)
+            Circle()
+                .trim(from: 0.2, to: 1.0)
+                .stroke(
+                    Color.gray,
+                    style: StrokeStyle(lineWidth: 5, lineCap: .round)
+                )
+                .frame(width: 40, height: 40)
+                .rotationEffect(.degrees(isAnimating ? 360 : 0))
+                .animation(
+                    .linear(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: isAnimating
+                )
+                .onAppear() {
+                    isAnimating = true
+                }
+        }
     }
 }
+
+#Preview {
+    loadingAnimation(isAnimating: false)
+}
+
