8f72308c64da27ab1dd6d88eac3b20883d8fd96f
Matthew Barton <matthewbarton2910@gmail.com>
2026-02-12 10:34:42 -0600

    added animation, im gonna move it to the app file

---
+++ VinBladel-Rework/LoadingAnimation.swift
@@ -0,0 +1,32 @@
+//
+//  LoadingAnimation.swift
+//  VinBladel-Rework
+//
+//  Created by Matthew S. Barton on 2/10/26.
+//
+
+
+import SwiftUI
+
+struct loadingAnimation: View {
+    @State var isAnimating = false
+    var body: some View {
+        
+        Circle()
+            .trim(from: 0.2, to: 1.0)
+            .stroke(
+                Color.gray,
+                style: StrokeStyle(lineWidth: 5, lineCap: .round)
+            )
+            .frame(width: 40, height: 40)
+            .rotationEffect(.degrees(isAnimating ? 360 : 0))
+            .animation(
+                .linear(duration: 1)
+                .repeatForever(autoreverses: false),
+                value: isAnimating
+            )
+            .onAppear() {
+                isAnimating = true
+            }
+    }
+}
--- VinBladel-Rework/StartPage.swift
+++ VinBladel-Rework/StartPage.swift
@@ -17,38 +17,46 @@ struct StartPage: View {
                 HStack {
                     NavigationLink(destination: InvoiceView()) {
                         Text("Test")
+                            .frame(width: 100, height: 50)
+                            .foregroundStyle(.black)
+                            .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
+                            .padding()
                     }
                     NavigationLink(destination: Existing()) {
                         Text("Existing")
+                            .frame(width: 100, height: 50)
+                            .foregroundStyle(.black)
+                            .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
+                            .padding()
                     }
-                    .frame(width: 100, height: 50)
-                    .foregroundStyle(.black)
-                    .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
-                    .padding()
+                   
                     NavigationLink(destination: InProgress()) {
                         Text("In Progress")
+                            .frame(width: 100, height: 50)
+                            .foregroundStyle(.black)
+                            .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
+                            .padding()
                     }
-                    .frame(width: 100, height: 50)
-                    .foregroundStyle(.black)
-                    .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
-                    .padding()
+                    
                 }
                 HStack {
                     
                     NavigationLink(destination: Complete()) {
                         Text("Completed")
+                            .frame(width: 100, height: 50)
+                        .foregroundStyle(.black)
+                        .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
+                        .padding()
                     }
-                    .frame(width: 100, height: 50)
-                    .foregroundStyle(.black)
-                    .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
-                    .padding()
+                    
                     NavigationLink(destination: AddVINView(scannedVIN: $scannedVin)) {
                         Text("Scan Vin")
+                            .frame(width: 100, height: 50)
+                            .foregroundStyle(.black)
+                            .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
+                            .padding()
                     }
-                    .frame(width: 100, height: 50)
-                    .foregroundStyle(.black)
-                    .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
-                    .padding()
+                    
                 }
             }
             .scaleEffect(2)
