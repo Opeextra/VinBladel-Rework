ac92701f679c17855f3072703fe610db4fc2cf2d
Matthew Barton <matthewbarton2910@gmail.com>
2026-02-23 11:17:28 -0600

    made the button animatios

--- VinBladel-Rework/StartPage.swift
+++ VinBladel-Rework/StartPage.swift
@@ -10,56 +10,98 @@ import SwiftUI
 struct StartPage: View {
 //    @Environment(DatabaseViewModel.self) private var viewModel
     @State var scannedVin: String? = nil
+    @State var isPressed1 = false
+    @State var isPressed2 = false
+    @State var isPressed3 = false
+    @State var isPressed4 = false
+    @State var isPressed5 = false
+    @State var isPressed6 = false
     var body: some View {
         NavigationStack{
             HStack{
                 VStack {
                     NavigationLink(destination: InvoiceView()) {
                         Text("Invoice Test")
-                            .frame(width: 100, height: 25)
                             .foregroundStyle(.black)
+                            .frame(width: 100, height: 25)
                             .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
-                            .padding()
+                            .scaleEffect(isPressed1 ? 0.5 : 1.0)
+                            .animation(.easeInOut(duration: 0.4), value: isPressed1)
                     }
+                    .simultaneousGesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { _ in isPressed1 = true }
+                            .onEnded { _ in isPressed1 = false }
+                    )
                     NavigationLink(destination: Existing()) {
                         Text("Existing")
-                            .frame(width: 100, height: 25)
                             .foregroundStyle(.black)
+                            .frame(width: 100, height: 25)
                             .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
+                            .scaleEffect(isPressed2 ? 0.5 : 1.0)
+                            .animation(.easeInOut(duration: 0.4), value: isPressed2)
                     }
+                    .simultaneousGesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { _ in isPressed2 = true }
+                            .onEnded { _ in isPressed2 = false }
+                    )
                    
                     NavigationLink(destination: InProgress()) {
                         Text("In Progress")
-                            .frame(width: 100, height: 25)
                             .foregroundStyle(.black)
+                            .frame(width: 100, height: 25)
                             .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
-                            .padding()
+                            .scaleEffect(isPressed3 ? 0.5 : 1.0)
+                            .animation(.easeInOut(duration: 0.4), value: isPressed3)
                     }
+                    .simultaneousGesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { _ in isPressed3 = true }
+                            .onEnded { _ in isPressed3 = false }
+                    )
                     
                 }
                 VStack {
                     NavigationLink(destination: Complete()) {
                         Text("Completed")
+                            .foregroundStyle(.black)
                             .frame(width: 100, height: 25)
-                        .foregroundStyle(.black)
-                        .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
-                        .padding()
+                            .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
+                            .scaleEffect(isPressed4 ? 0.5 : 1.0)
+                            .animation(.easeInOut(duration: 0.4), value: isPressed4)
                     }
-                    
+                    .simultaneousGesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { _ in isPressed4 = true }
+                            .onEnded { _ in isPressed4 = false }
+                    )
                     NavigationLink(destination: AddVINView(scannedVIN: $scannedVin)) {
                         Text("Scan Vin")
-                            .frame(width: 100, height: 25)
                             .foregroundStyle(.black)
+                            .frame(width: 100, height: 25)
                             .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
+                            .scaleEffect(isPressed5 ? 0.5 : 1.0)
+                            .animation(.easeInOut(duration: 0.4), value: isPressed5)
                     }
+                    .simultaneousGesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { _ in isPressed5 = true }
+                            .onEnded { _ in isPressed5 = false }
+                    )
                     NavigationLink(destination: PartsandServices()) {
                         Text("Services")
-                            .frame(width: 100, height: 25)
                             .foregroundStyle(.black)
+                            .frame(width: 100, height: 25)
                             .background(RoundedRectangle(cornerRadius: 10).foregroundStyle(.orange))
-                            .padding()
-                            
+                            .scaleEffect(isPressed6 ? 0.5 : 1.0)
+                            .animation(.easeInOut(duration: 0.4), value: isPressed6)
                     }
+                    .simultaneousGesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { _ in isPressed6 = true }
+                            .onEnded { _ in isPressed6 = false }
+                    )
                 }
             }
             .scaleEffect(3)
