4e13e1a90a2bafa1e8c32686077c3043823e8ff8
LuffyGear5 <ashah8399@stu.d214.org>
2026-02-25 10:40:06 -0600

    making app in light mode

--- VinBladel-Rework/VinBladel_ReworkApp.swift
+++ VinBladel-Rework/VinBladel_ReworkApp.swift
@@ -9,25 +9,28 @@ import SwiftUI
 
 @main
 struct VinBladel_ReworkApp: App {
-    @State var lauchScreen: Bool = false
+    @State var launchScreen: Bool = false
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            if lauchScreen == true {
-                StartPage()
-//                ContentView()
-                    .transition(.slide)
-            } else {
-                loadingAnimation()
-                    .onAppear(){
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                            withAnimation() {
-                                self.lauchScreen = true
-                                // Make it so the firebase realtime database loads during this time
+            Group {
+                if launchScreen == true {
+                    StartPage()
+                        .transition(.slide)
+                } else {
+                    loadingAnimation()
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                withAnimation {
+                                    self.launchScreen = true
+                                    // Make it so the firebase realtime database loads during this time
+                                }
                             }
                         }
-                    }
+                }
             }
+            .preferredColorScheme(.light)
         }
     }
+        
 }
